9bc382db9b18dfa7c92d5e4e583e02f3325d2bab
Royaltie-byte <186587026+Royaltie-byte@users.noreply.github.com>
2026-07-19 20:59:10 +0300

    add enrollment crud routes , and controller function shells

---
+++ backend/server/src/controllers/enrollment.controller.ts
@@ -0,0 +1,26 @@
+import type { Request , Response } from 'express';
+
+//========== POST /enrollment route ============//
+
+export const createEnrollment = async (req: Request , res: Response ) => {
+
+}
+
+
+//========== GET /enrollment route ============//
+
+export const listEnrollments = async (req: Request , res: Response ) => {
+
+}
+
+//=========== GET /enrollment/:id ===== ===//
+
+export const getEnrollment = async ( req: Request , res: Response ) => {
+
+}
+
+//========== PATCH /enrollment/:id ===========//
+
+export const updateEnrollment = async (req: Request , res: Response ) => {
+    
+}
