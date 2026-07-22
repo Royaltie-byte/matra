032570bc0187bd4cf30e8602b52bfb71849ca484
Kenneth <jatani1@duck.com>
2026-07-22 20:12:05 +0300

    Simple, beginning page layout architecture

--- frontend/matra-frontend/src/layouts/DashboardLayout.tsx
+++ frontend/matra-frontend/src/layouts/DashboardLayout.tsx
@@ -1,1 +1,32 @@
+import { Outlet } from "react-router-dom";
 
+import Navbar from "../components/layout/Navbar";
+import Sidebar from "../components/layout/Sidebar";
+
+function DashboardLayout() {
+  return (
+    <>
+      <Navbar />
+
+      <div
+        style={{
+          display: "flex",
+          minHeight: "calc(100vh - 60px)",
+        }}
+      >
+        <Sidebar />
+
+        <main
+          style={{
+            flex: 1,
+            padding: "30px",
+          }}
+        >
+          <Outlet />
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default DashboardLayout;
--- frontend/matra-frontend/src/pages/settings/Settings.tsx
+++ frontend/matra-frontend/src/pages/settings/Settings.tsx
@@ -1,5 +1,5 @@
 function Login() {
-  return <h1>Dashboard</h1>;
+  return <h1>Settings</h1>;
 }
 
 export default Login;
